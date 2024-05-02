2cecf3555fe0332de06df8af6eeba8570db622b2
Teddo <twa2509@gmail.com>
2024-05-02 10:15:42 +0000

    Spring

---
+++ QCM_PLUS/src/main/java/com/PFE/QCM_PLUS/QcmPlusApplication.java
@@ -0,0 +1,13 @@
+package com.PFE.QCM_PLUS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcmPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QcmPlusApplication.class, args);
+	}
+
+}
---
+++ QCM_PLUS/src/test/java/com/PFE/QCM_PLUS/QcmPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PFE.QCM_PLUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcmPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
