6500ec70d29a68b2e635c4f73c175777d24ad050
shamss12301230 <shamss12301230>
2024-05-29 14:15:37 +0200

    Update the mockQuestionnaire

--- front/src/assets/mockQuestionnaire.ts
+++ front/src/assets/mockQuestionnaire.ts
@@ -1,30 +1,30 @@
 import { QuestionnaireI } from "../interfaces/QuestionnaireI";
 
-const questionnaire: QuestionnaireI[] = [
+const mockQuestionnaire: QuestionnaireI[] = [
     {
         id: 1,
-        title: "TypeScript",
-        description:
-            "TypeScript est un langage de programmation libre et open source développé par Microsoft qui a pour but d'améliorer et de sécuriser la production de code JavaScript. Il s'agit d'un sur-ensemble syntaxique strict de JavaScript.",
+        title: "TypeScript Basics",
+        description: "Testez vos connaissances de base en TypeScript.",
+        questions: [1, 2],
     },
     {
         id: 2,
-        title: "Java",
-        description:
-            "Java est un langage de programmation de haut niveau orienté objet créé par James Gosling et Patrick Naughton, employés de Sun Microsystems, avec le soutien de Bill Joy, présenté officiellement le 23 mai 1995 au SunWorld.",
+        title: "Java Basics",
+        description: "Testez vos connaissances de base en Java.",
+        questions: [3, 4],
     },
     {
         id: 3,
-        title: "Python",
-        description:
-            "Python est un langage de programmation interprété, multiparadigme et multiplateformes. Il favorise la programmation impérative structurée, fonctionnelle et orientée objet.",
+        title: "Python Basics",
+        description: "Testez vos connaissances de base en Python.",
+        questions: [5, 6],
     },
     {
         id: 4,
-        title: "Haskell",
-        description:
-            "Haskell est un langage de programmation fonctionnel fondé sur le lambda-calcul et la logique combinatoire.",
+        title: "Haskell Basics",
+        description: "Testez vos connaissances de base en Haskell.",
+        questions: [7, 8],
     },
 ];
 
-export default questionnaire;
+export default mockQuestionnaire;
